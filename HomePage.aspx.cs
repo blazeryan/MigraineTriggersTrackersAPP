982d629d88fe3ef229a449fdc88acde042d0fd4b
blazeryan <blazeryan21@gmail.com>
2022-07-11 20:36:53 -0400

    Updated Add Sleep Button Convert Selected Hours to Minutes Add converted minutes to Selected Minutes.

--- HomePage.aspx.cs
+++ HomePage.aspx.cs
@@ -11,21 +11,21 @@ namespace MigraineTriggersTrackersAPP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
 
         protected void TimeDropdownList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
-     
+
         protected void SleepDropDownList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-    
+
         protected void FoodDropDownList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -73,29 +73,44 @@ namespace MigraineTriggersTrackersAPP
 
         protected void AddSleepButton_Click(object sender, EventArgs e)
         {
-             MigraineListBox.Items.Add(SleepDropDownList.SelectedItem.ToString());
 
-            // Add Minutes Drop Down Only if Hours Drop Down is Not Selected
-            if (HoursDropDownList.SelectedIndex != 0)
-            {
-                TimeListBox.Items.Add(HoursDropDownList.SelectedItem.ToString());
-            }
-            else
-            {
-                TimeListBox.Items.Add(MinutesDropDownList.SelectedItem.ToString());
-            }
+
+            MigraineListBox.Items.Add(SleepDropDownList.SelectedItem.ToString());
+
+
             IntensityListBox.Items.Add(IntensityDropDownList.SelectedItem.ToString());
             QuantityListBox.Items.Add(QuantityDropDownList.SelectedItem.ToString());
 
             // Reset DropDownLists to Index 0 or NA
             SymptomsDropDownList.SelectedIndex = 0;
-            HoursDropDownList.SelectedIndex = 0;
-            MinutesDropDownList.SelectedIndex = 0;
+            // HoursDropDownList.SelectedIndex = 0;
+            //MinutesDropDownList.SelectedIndex = 0;
             IntensityDropDownList.SelectedIndex = 0;
             QuantityDropDownList.SelectedIndex = 0;
             FoodDropDownList.SelectedIndex = 0;
             SleepDropDownList.SelectedIndex = 0;
             DrinksDropDownList.SelectedIndex = 0;
+
+
+            string hoursSelected = "";
+            string minutesSelected = "";
+
+
+            // Convert Selected DropDown Hours to Minutes & add them to Selected Minutes
+            hoursSelected = HoursDropDownList.SelectedValue.ToString();
+
+            minutesSelected = MinutesDropDownList.SelectedValue.ToString();
+
+            int hoursSelectedToInt = Convert.ToInt32(hoursSelected);
+
+            hoursSelectedToInt = hoursSelectedToInt * 60;
+
+            int minutesSelectedToInt = Convert.ToInt32(minutesSelected);
+
+            int totalMinutes = hoursSelectedToInt + minutesSelectedToInt;
+
+            TimeListBox.Items.Add(totalMinutes.ToString());
+
         }
 
         protected void SymptomDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
@@ -108,7 +123,7 @@ namespace MigraineTriggersTrackersAPP
 
         }
 
-       
+
 
         protected void SymptomsDropdownList_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -143,7 +158,7 @@ namespace MigraineTriggersTrackersAPP
 
         protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void TimeListBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -206,7 +221,15 @@ namespace MigraineTriggersTrackersAPP
         protected void HoursDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
         {
 
+
+
+
+
         }
+
+
+
+
 
         protected void MinutesDropDownList_SelectedIndexChanged(object sender, EventArgs e)
         {
