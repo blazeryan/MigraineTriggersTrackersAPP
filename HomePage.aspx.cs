039343df55da7cb2c11f6aa59281a9b19e3bd701
blazeryan <blazeryan21@gmail.com>
2022-07-11 21:07:36 -0400

    Finally have minutes and hours being added together and converted to total minutes even when NA is selected on either the hours or minutes!

--- HomePage.aspx.cs
+++ HomePage.aspx.cs
@@ -83,190 +83,218 @@ namespace MigraineTriggersTrackersAPP
 
             // Reset DropDownLists to Index 0 or NA
             SymptomsDropDownList.SelectedIndex = 0;
-          
+
             IntensityDropDownList.SelectedIndex = 0;
             QuantityDropDownList.SelectedIndex = 0;
             FoodDropDownList.SelectedIndex = 0;
             SleepDropDownList.SelectedIndex = 0;
             DrinksDropDownList.SelectedIndex = 0;
-        
 
-            string hoursSelected = "";
-            string minutesSelected = "";
 
+            string hoursSelected = "NA";
+            string minutesSelected = "NA";
+            int hoursSelectedToInt = 0;
+            int minutesSelectedToInt = 0;
+            int totalMinutes = 0;
 
             // Convert Selected DropDown Hours to Minutes & add them to Selected Minutes
-            hoursSelected = HoursDropDownList.SelectedValue.ToString();
 
-            minutesSelected = MinutesDropDownList.SelectedValue.ToString();
+            if (HoursDropDownList.SelectedIndex != 0)
+            {
+                hoursSelected = HoursDropDownList.SelectedValue.ToString();
+            }
 
-            int hoursSelectedToInt = Convert.ToInt32(hoursSelected);
+            if (MinutesDropDownList.SelectedIndex != 0)
+            {
+                minutesSelected = MinutesDropDownList.SelectedValue.ToString();
+            }
 
-            hoursSelectedToInt = hoursSelectedToInt * 60;
+            if (hoursSelected != "NA")
+            {
+                hoursSelectedToInt = Convert.ToInt32(hoursSelected);
+            }
 
-            int minutesSelectedToInt = Convert.ToInt32(minutesSelected);
+            if (hoursSelected != "NA")
+            {
+                hoursSelectedToInt = hoursSelectedToInt * 60;
+            }
 
-            int totalMinutes = hoursSelectedToInt + minutesSelectedToInt;
+            if (minutesSelected != "NA")
+            {
+                minutesSelectedToInt = Convert.ToInt32(minutesSelected);
 
-            TimeListBox.Items.Add(totalMinutes.ToString());
+            }
+
+            if (hoursSelectedToInt != 0)
+            { 
+                   totalMinutes = hoursSelectedToInt;
+            }
+        
+            if (minutesSelectedToInt != 0)
+            {
+                    totalMinutes = totalMinutes + minutesSelectedToInt;
+            }
+
+
+    TimeListBox.Items.Add(totalMinutes.ToString());
 
             // Reset Hours and Minutes DropDownLists after Selected Hours & Times have been populated onto List Box
             HoursDropDownList.SelectedIndex = 0;
             MinutesDropDownList.SelectedIndex = 0;
-        }
+            }
 
-        protected void SymptomDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
+protected void SymptomDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+{
 
-        }
+}
 
-        protected void MigraineListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
+protected void MigraineListBox_SelectedIndexChanged(object sender, EventArgs e)
+{
 
-        }
-
-
-
-        protected void SymptomsDropdownList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-        protected void QuantityDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void IntensityDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void NotesTextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void SymptomsDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
-
-        }
-
-        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void TimeListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void QuantityListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void DrinksDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
-
-        }
-
-        protected void AddFoodsButton_Click(object sender, EventArgs e)
-        {
-            MigraineListBox.Items.Add(FoodDropDownList.SelectedItem.ToString());
-            TimeListBox.Items.Add(HoursDropDownList.SelectedItem.ToString());
-            IntensityListBox.Items.Add(IntensityDropDownList.SelectedItem.ToString());
-            QuantityListBox.Items.Add(QuantityDropDownList.SelectedItem.ToString());
-
-            // Reset DropDownLists to Index 0 or NA
-            SymptomsDropDownList.SelectedIndex = 0;
-            HoursDropDownList.SelectedIndex = 0;
-            MinutesDropDownList.SelectedIndex = 0;
-            IntensityDropDownList.SelectedIndex = 0;
-            QuantityDropDownList.SelectedIndex = 0;
-            FoodDropDownList.SelectedIndex = 0;
-            SleepDropDownList.SelectedIndex = 0;
-            DrinksDropDownList.SelectedIndex = 0;
-        }
-
-        protected void AddDrinksButton_Click(object sender, EventArgs e)
-        {
-            MigraineListBox.Items.Add(DrinksDropDownList.SelectedItem.ToString());
-            TimeListBox.Items.Add(HoursDropDownList.SelectedItem.ToString());
-            IntensityListBox.Items.Add(IntensityDropDownList.SelectedItem.ToString());
-            QuantityListBox.Items.Add(QuantityDropDownList.SelectedItem.ToString());
-
-            // Reset DropDownLists to Index 0 or NA
-            SymptomsDropDownList.SelectedIndex = 0;
-            HoursDropDownList.SelectedIndex = 0;
-            MinutesDropDownList.SelectedIndex = 0;
-            IntensityDropDownList.SelectedIndex = 0;
-            QuantityDropDownList.SelectedIndex = 0;
-            FoodDropDownList.SelectedIndex = 0;
-            SleepDropDownList.SelectedIndex = 0;
-            DrinksDropDownList.SelectedIndex = 0;
-        }
-
-
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void HoursDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
-        {
+}
 
 
 
+protected void SymptomsDropdownList_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
 
 
-        }
+protected void QuantityDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
+{
+
+}
+
+protected void IntensityDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
+{
+
+}
+
+protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void NotesTextBox_TextChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void SymptomsDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+{
+
+}
+
+protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void TimeListBox_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void QuantityListBox_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void DrinksDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+{
+
+}
+
+protected void AddFoodsButton_Click(object sender, EventArgs e)
+{
+    MigraineListBox.Items.Add(FoodDropDownList.SelectedItem.ToString());
+    TimeListBox.Items.Add(HoursDropDownList.SelectedItem.ToString());
+    IntensityListBox.Items.Add(IntensityDropDownList.SelectedItem.ToString());
+    QuantityListBox.Items.Add(QuantityDropDownList.SelectedItem.ToString());
+
+    // Reset DropDownLists to Index 0 or NA
+    SymptomsDropDownList.SelectedIndex = 0;
+    HoursDropDownList.SelectedIndex = 0;
+    MinutesDropDownList.SelectedIndex = 0;
+    IntensityDropDownList.SelectedIndex = 0;
+    QuantityDropDownList.SelectedIndex = 0;
+    FoodDropDownList.SelectedIndex = 0;
+    SleepDropDownList.SelectedIndex = 0;
+    DrinksDropDownList.SelectedIndex = 0;
+}
+
+protected void AddDrinksButton_Click(object sender, EventArgs e)
+{
+    MigraineListBox.Items.Add(DrinksDropDownList.SelectedItem.ToString());
+    TimeListBox.Items.Add(HoursDropDownList.SelectedItem.ToString());
+    IntensityListBox.Items.Add(IntensityDropDownList.SelectedItem.ToString());
+    QuantityListBox.Items.Add(QuantityDropDownList.SelectedItem.ToString());
+
+    // Reset DropDownLists to Index 0 or NA
+    SymptomsDropDownList.SelectedIndex = 0;
+    HoursDropDownList.SelectedIndex = 0;
+    MinutesDropDownList.SelectedIndex = 0;
+    IntensityDropDownList.SelectedIndex = 0;
+    QuantityDropDownList.SelectedIndex = 0;
+    FoodDropDownList.SelectedIndex = 0;
+    SleepDropDownList.SelectedIndex = 0;
+    DrinksDropDownList.SelectedIndex = 0;
+}
+
+
+protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+{
+
+}
+
+protected void HoursDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
+{
 
 
 
 
 
-        protected void MinutesDropDownList_SelectedIndexChanged(object sender, EventArgs e)
-        {
+}
 
-        }
 
-        protected void TimeDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
 
-        }
 
-        protected void MinutesDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
 
-        }
+protected void MinutesDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+{
 
-        protected void SleepDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
-        {
+}
 
-        }
+protected void TimeDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+{
 
-        protected void ClearLastItemButton_Click(object sender, EventArgs e)
-        {
-            MigraineListBox.Items.RemoveAt(MigraineListBox.Items.Count - 1);
-            TimeListBox.Items.RemoveAt(TimeListBox.Items.Count - 1);
-            IntensityListBox.Items.RemoveAt(IntensityListBox.Items.Count - 1);
-            QuantityDropDownList.Items.RemoveAt(QuantityDropDownList.Items.Count - 1);
-        }
+}
 
-        protected void RemoveLastButton_Click(object sender, EventArgs e)
-        {
-            MigraineListBox.Items.RemoveAt(MigraineListBox.Items.Count - 1);
-            TimeListBox.Items.RemoveAt(TimeListBox.Items.Count - 1);
-            IntensityListBox.Items.RemoveAt(IntensityListBox.Items.Count - 1);
-            QuantityListBox.Items.RemoveAt(QuantityDropDownList.Items.Count - 1);
-        }
+protected void MinutesDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+{
+
+}
+
+protected void SleepDropDownList_SelectedIndexChanged1(object sender, EventArgs e)
+{
+
+}
+
+protected void ClearLastItemButton_Click(object sender, EventArgs e)
+{
+    MigraineListBox.Items.RemoveAt(MigraineListBox.Items.Count - 1);
+    TimeListBox.Items.RemoveAt(TimeListBox.Items.Count - 1);
+    IntensityListBox.Items.RemoveAt(IntensityListBox.Items.Count - 1);
+    QuantityDropDownList.Items.RemoveAt(QuantityDropDownList.Items.Count - 1);
+}
+
+protected void RemoveLastButton_Click(object sender, EventArgs e)
+{
+    MigraineListBox.Items.RemoveAt(MigraineListBox.Items.Count - 1);
+    TimeListBox.Items.RemoveAt(TimeListBox.Items.Count - 1);
+    IntensityListBox.Items.RemoveAt(IntensityListBox.Items.Count - 1);
+    QuantityListBox.Items.RemoveAt(QuantityDropDownList.Items.Count - 1);
+}
     }
 }
